a5f9dca0af5b46b571d9036b4e301e0e9c482db7
Trex099 <arshgour16@gmail.com>
2025-04-20 06:56:23 -0700

    Initial Commit

---
+++ Finity/FinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinityApp.swift
+//  Finity
+//
+//  Created by arsh on 20/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinityTests/FinityTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinityTests.swift
+//  FinityTests
+//
+//  Created by arsh on 20/04/25.
+//
+
+import Testing
+@testable import Finity
+
+struct FinityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
